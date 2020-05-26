d1d405d05c18a66f61d942aae6be8de8bd375899
Rodrik Klesse <rodrik_hsk@hotmail.com>
2020-05-26 16:28:30 -0300

    Created routes and organized code with controllers

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({
-        evento: 'Semana OmniStack 11.0',
-        aluno: 'Rodrik Klesse'
-    });
-});
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
